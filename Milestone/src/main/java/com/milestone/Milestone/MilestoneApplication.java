a5c46f82db3bdc3a4db5068a7892f5c3a930eb24
TamenD <jj794001@example.com>
2021-09-06 21:11:26 +0530

    Changed from jar to war

--- Milestone/src/main/java/com/milestone/Milestone/MilestoneApplication.java
+++ Milestone/src/main/java/com/milestone/Milestone/MilestoneApplication.java
@@ -2,12 +2,19 @@ package com.milestone.Milestone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 
 @SpringBootApplication
-public class MilestoneApplication {
+public class MilestoneApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MilestoneApplication.class, args);
 	}
-
+	@Override
+	protected SpringaApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MilestoneApplication.class);
+	}
 }
